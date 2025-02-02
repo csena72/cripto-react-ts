7db71c1896d536ffe493800710a5a20edebf61cf
Cristian <cristianjsena@gmail.com>
2025-02-02 15:13:28 +0000

    Agrego paises

--- src/data/index.ts
+++ src/data/index.ts
@@ -2,7 +2,11 @@ import { Currency } from "../types";
 
 export const currencies : Currency[] = [
   { code: 'USD', name: 'Dolar de Estados Unidos'},
-  { code: 'MXN', name: 'Peso Mexicano'},
+  { code: 'CAD', name: 'Dolar Canadiense'},
   { code: 'EUR', name: 'Euro'},
   { code: 'GBP', name: 'Libra Esterlina'},
+  { code: 'ARS', name: 'Peso Argentino'},
+  { code: 'MXN', name: 'Peso Mexicano'},
+  { code: 'BRL', name: 'Real Brasileño'},
+  { code: 'CLP', name: 'Peso Chileno'},
 ]
